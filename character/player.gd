327b8b2b70c6f282f5874162867f2e54775b7e30
RonanDorn <evgeny.2001.khom@gmail.com>
2023-11-27 22:25:38 +0400

    LittleCodeChanges

--- character/player.gd
+++ character/player.gd
@@ -4,24 +4,22 @@ enum STATE { MOVEMENT, JUMP, WALLSLIDE, DEATH }
 
 @onready var coyote_timer		 = $Timers/CoyoteTimer
 @onready var jump_buffer		 = $Timers/JumpBuffer
-@onready var wall_slide_timer	 = $Timers/WallSlideTimer
-@onready var sliding_timer		 = $Timers/SlidingTimer
-@onready var checkpoint = self.position
-
 @onready var anim				 = $AnimatedSprite2D
 
-@export var speed = 150
-@export var acceleration = 400
-@export var friction = 900
-@export var jump_short_impulse = -250
-@export var jump_impulse = -250
-@export var air_acceleration = 300
-@export var air_resistance = 900
-@export var terminal_gravity = 1450
-@export var extra_gravity = 150
-@export var slide_speed = 400
-@export var slide_acceleration = 50
-@export var wall_jump_impulse = Vector2(45, -250)
+@onready var checkpoint			 = self.position
+
+@export var speed				 = 150
+@export var acceleration		 = 400
+@export var friction			 = 900
+@export var jump_short_impulse	 = -250
+@export var jump_impulse		 = -250
+@export var air_acceleration	 = 300
+@export var air_resistance		 = 700
+@export var terminal_gravity	 = 1200
+@export var extra_gravity		 = 150
+@export var slide_speed			 = 400
+@export var slide_acceleration	 = 50
+@export var wall_jump_impulse	 = Vector2(45, -250)
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -29,8 +27,6 @@ var direction
 var current_state = STATE.MOVEMENT
 
 var has_second_jump		 = true
-var has_sliding			 = true
-var is_jumping = false
 var is_short_jump
 var was_on_floor
 
@@ -41,8 +37,6 @@ func _ready():
 func _physics_process(delta):
 	direction = Input.get_axis("left", "right")
 	state_update(delta)
-	if sliding_timer.time_left == 0:
-		has_sliding = true
 	
 	was_on_floor = is_on_floor()
 	
@@ -62,7 +56,6 @@ func movement(delta):
 	if is_on_floor() or coyote_timer.time_left > 0:
 		
 		if jump_buffer.time_left > 0 and !is_short_jump:
-			is_jumping = true
 			velocity.y = jump_impulse
 		
 		has_second_jump = true
@@ -81,27 +74,23 @@ func movement(delta):
 			
 		if Input.is_action_just_pressed("jump") and GameData.high_jump:
 			anim.play("jump")
-			is_jumping = true
 			velocity.y = jump_impulse
 			current_state = STATE.JUMP
 		
 		elif Input.is_action_just_pressed("jump") and !GameData.high_jump:
 			anim.play("jump")
-			is_jumping = true
 			is_short_jump = true
 			velocity.y = jump_short_impulse
 			current_state = STATE.JUMP
 			
 	elif !is_on_floor() and coyote_timer.time_left == 0:
 		current_state = STATE.JUMP
-		is_jumping = false
 		
 func jump(delta):
 	if !is_on_floor():
 		
 		if Input.is_action_just_released("jump") and !is_on_floor() and velocity.y < jump_short_impulse:
 			anim.play("jump")
-			is_jumping = true
 			is_short_jump = true
 			velocity.y = jump_short_impulse
 			
@@ -123,18 +112,20 @@ func jump(delta):
 		if velocity.y > -60:
 			anim.play("fall")		
 		
-		if velocity.y > gravity / 2:
+		if velocity.y > 0:
+			velocity.y += (gravity/10) * delta
+		
+		if velocity.y > gravity / 1.5:
 			velocity.y += (gravity + extra_gravity) * delta
 			
-		if is_on_wall() and has_sliding and wall_slide_timer.time_left == 0 and velocity.y > 0:
-			sliding_timer.start()
+		if is_on_wall() and velocity.y > 0:
 			current_state = STATE.WALLSLIDE
 			
 	elif is_on_floor():
 		current_state = STATE.MOVEMENT
 
 func wallslide(delta):
-	if !is_on_floor() and is_on_wall() and has_sliding:
+	if !is_on_floor() and is_on_wall() and direction != get_wall_normal().x:
 		
 		anim.play("wallSlide")
 		
@@ -151,27 +142,21 @@ func wallslide(delta):
 			velocity.y = wall_jump_impulse.y
 			anim.play("wallJump")
 			current_state = STATE.JUMP
-			
-		if sliding_timer.time_left == 0:
-			has_sliding = false
-			wall_slide_timer.start()
-			anim.play("fall")
-			current_state = STATE.JUMP
 	
-	elif is_on_floor():
+	else:
 		current_state = STATE.MOVEMENT
-		
-	elif !is_on_floor() and !is_on_wall():
-		current_state = STATE.JUMP
 	
 func death():
+	velocity = Vector2.ZERO
 	anim.play("death")
 	await anim.animation_finished
 	self.global_position = checkpoint
 	current_state = STATE.MOVEMENT
 
 func hit_box():
-	current_state = STATE.DEATH
+	if current_state != STATE.DEATH:
+		current_state = STATE.DEATH
 
 func checkpoint_update(checkpoint_position):
-	checkpoint = checkpoint_position
+	if checkpoint !=checkpoint_position:
+		checkpoint = checkpoint_position
